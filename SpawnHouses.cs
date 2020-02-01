016e10aa1f20f4f84850f7080a0875e836701a4e
Ivan Diakov <ivandiakov@MacBook-Pro-Ivan.local>
2020-02-01 13:54:00 +0200

    Houses spawning script

---
+++ SpawnHouses.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHouses : MonoBehaviour
+{
+    public GameObject house;
+    float randX;
+    Vector2 SpawnPlace;
+    public float SpawnRate = 5f;
+    float NextSpawn = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Time.time > NextSpawn)
+        {
+            NextSpawn = Time.time + SpawnRate;
+            randX = Random.Range(-8.4f, 8.4f);
+            SpawnPlace = new Vector2(randX, transform.position.y);
+            Instantiate(house, SpawnPlace, Quaternion.identity);
+        }
+    }
+}
