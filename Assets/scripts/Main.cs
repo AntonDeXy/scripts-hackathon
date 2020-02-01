fabb1023490e4fcbba99977679f0ac6a48b4dd58
AntonDeXy <podolyananton@gmail.com>
2020-02-01 13:58:19 +0200

    chgs

---
+++ Assets/scripts/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    public static int coins;
+    public Text coinsText;
+    public List<Skin> shopItems = new List<Skin>();
+    public Text[] shopItemsText;
+    public List<Skin> boughtedSkins = new List<Skin>();
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        updateCoinsCount();
+    }
+
+    public void updateCoinsCount ()
+    {
+        coinsText.text = coins.ToString();
+    }
+}
+
+[Serializable]
+public class Skin {
+    public GameObject skin;
+    public int price;
+    public GameObject button;
+}
